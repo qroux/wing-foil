b61cd22728f24cb6c9837537847c588d66b27519
qroux <quentin.roux@hotmail.fr>
2021-10-27 18:45:17 +0200

    animatedPage tryout

--- pages/_animatedPage.tsx
+++ pages/_animatedPage.tsx
@@ -8,15 +8,15 @@ export default function AnimatedPage({ children }: { children: ReactNode }) {
     <motion.div
       initial={{
         opacity: 0,
-        y: '0.5rem',
+        // y: '0.5rem',
       }}
       animate={{
         opacity: 1,
-        y: 0,
+        // y: 0,
       }}
       exit={{
         opacity: 0,
-        y: '0.5rem',
+        // y: '0.5rem',
       }}>
       {children}
     </motion.div>

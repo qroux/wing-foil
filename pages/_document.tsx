87a478c76cf91d6d7d0b016e5b0d4f6387342ebb
qroux <quentin.roux@hotmail.fr>
2021-11-09 18:11:58 +0100

    GA implemented

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ import createEmotionServer from '@emotion/server/create-instance';
 import theme from '../styles/theme/theme';
 import createEmotionCache from '../styles/theme/createEmotionCache';
 import { SimplePaletteColorOptions } from '@mui/material';
+import { GA_TRACKING_ID } from '../lib/gtag';
 
 export default class MyDocument extends Document {
   render() {
@@ -16,6 +17,23 @@ export default class MyDocument extends Document {
           <link
             href='https://fonts.googleapis.com/css2?family=Urbanist:wght@300;400;500;700&display=swap'
             rel='stylesheet'
+          />
+          {/* GA */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
           />
         </Head>
         <body>
